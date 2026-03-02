93c3657ed1903293100425f846e0c136b89fecb0
dhaerieshan <dhaerieshan@gmail.com>
2026-03-02 17:01:41 +0530

    test updated

--- src/test/java/com/borneo/ecommerce/repository/UserRepositoryTest.java
+++ src/test/java/com/borneo/ecommerce/repository/UserRepositoryTest.java
@@ -11,12 +11,14 @@ import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 class UserRepositoryTest {
 
   @Autowired private UserRepository userRepository;
